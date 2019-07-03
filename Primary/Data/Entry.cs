3a52148d1d77edf7bf16e1e82e40803f9b533b0d
Juan Manuel Alvarez <naicigam@gmail.com>
2019-07-03 19:34:13 -0300

    Market data Entry enumeration and serialization.

--- Primary/Data/Entry.cs
+++ Primary/Data/Entry.cs
@@ -1,40 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using Newtonsoft.Json;
 using Primary.Serialization;
 
 namespace Primary.Data
 {
-    [TypeConverter(typeof(EntryToStringConverter))]
-    public class Entry : ValueType
+    [JsonConverter(typeof(EntryJsonSerializer))]
+    public enum Entry
     {
-        public static Entry Bids = new Entry("Bids");
-        public static Entry Offers = new Entry("Offers");
-        //public static Entry Last,
-        //public static Entry Open,
-        //public static Entry Close,
-        //public static Entry SettlementPrice,
-        //public static Entry SessionHighPrice,
-        //public static Entry SessionLowPrice,
-        //public static Entry Volume,
-        //public static Entry OpenInterest,
-        //public static Entry IndexValue,
-        //public static Entry EffectiveVolume,
-        //public static Entry NominalVolume
-
-        public Entry(string value) : base(value)
-        {
-        }
+        Bids, 
+        Offers,
+        Last,
+        Open,
+        Close,
+        SettlementPrice,
+        SessionHighPrice,
+        SessionLowPrice,
+        Volume,
+        OpenInterest,
+        IndexValue,
+        EffectiveVolume,
+        NominalVolume
     } 
 
     #region String serialization
 
-    public class EntryToStringConverter : EnumToStringConverter<Entry>
+    public class EntryJsonSerializer : EnumJsonSerializer<Entry>
     {
         protected override Dictionary<Entry, string> EnumToString =>
             new Dictionary<Entry, string>
             {
                 {Entry.Bids, "BI"},
-                {Entry.Offers, "OF"}
+                {Entry.Offers, "OF"},
+                {Entry.Last, "LA"},
+                {Entry.Open, "OP"},
+                {Entry.Close, "CL"},
+                {Entry.SettlementPrice, "SE"},
+                {Entry.SessionHighPrice, "HI"},
+                {Entry.SessionLowPrice, "LO"},
+                {Entry.Volume, "TV"},
+                {Entry.OpenInterest, "OI"},
+                {Entry.IndexValue, "IV"},
+                {Entry.EffectiveVolume, "EV"},
+                {Entry.NominalVolume, "NV"}
             };
     }
 

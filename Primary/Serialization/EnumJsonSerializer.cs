e9e3b97e31fd2209082fd9486ff73aef27a019ee
Juan Manuel Alvarez <naicigam@gmail.com>
2019-07-03 19:34:36 -0300

    Enums JSON serialization.

---
+++ Primary/Serialization/EnumJsonSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Primary.Serialization
+{
+    public abstract class EnumJsonSerializer<T> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var valueToWrite = EnumToString[(T)value];
+            writer.WriteValue(valueToWrite);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return EnumToString.FirstOrDefault(x => x.Value == (string)existingValue).Key;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+        
+        protected abstract Dictionary<T, string> EnumToString { get; }
+    }
+}

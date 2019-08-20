399d4f66400bd762ee66ffc280732710774b279b
Juan Manuel Alvarez <naicigam@gmail.com>
2019-08-20 20:01:40 -0300

    Added OrderId.

---
+++ Primary/Data/Orders/OrderId.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Primary.Data.Orders
+{
+    public class OrderId
+    {
+        [JsonProperty("clientId")]
+        public ulong ClientId { get; set; }
+        
+        [JsonProperty("proprietary")]
+        public string Proprietary { get; set; }
+    }
+}

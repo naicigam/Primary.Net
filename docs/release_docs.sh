e8c4cc9fbba625465695fe0c02dcde00f0bcab5a
Juan Manuel Alvarez <naicigam@gmail.com>
2019-10-13 11:40:04 -0300

    Install DocFX console.

--- docs/release_docs.sh
+++ docs/release_docs.sh
@@ -4,7 +4,7 @@ set -e
 export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
 export VisualStudioVersion="15.0"
 
-#nuget install docfx -Version 2.44.0
+nuget install docfx
 nuget install docfx.console
 
 # https://github.com/dotnet/docfx/issues/3177

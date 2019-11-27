704790bb1a604adef552aa7cef02644ff587a7da
Juan Manuel Alvarez <naicigam@gmail.com>
2019-11-27 20:27:49 -0300

    Remove unused using directives.

--- Primary/OrderDataWebSocket.cs
+++ Primary/OrderDataWebSocket.cs
@@ -6,8 +6,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Primary.Data;
 using Primary.Data.Orders;
 
 namespace Primary

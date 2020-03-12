c514868a01146b9d0853563facbd9c6103ee21d5
Juan Manuel Alvarez <naicigam@gmail.com>
2020-03-12 17:41:14 -0300

    English documentation.

--- Primary.Examples/GetInstrumentsAndDataExample.cs
+++ Primary.Examples/GetInstrumentsAndDataExample.cs
@@ -6,20 +6,23 @@ namespace Primary.Examples
 {
     internal static class GetInstrumentsAndDataExample
     {
-        private static async Task RunExample()
+        private static async Task Main()
         {
-            // Login
+            Console.WriteLine("Connecting to ReMarkets...");
+            
             var api = new Api(Api.DemoEndpoint);
             await api.Login(Api.DemoUsername, Api.DemoPassword);
 
-            // Obtener instrumento a operar
+            // Get instruments
+            Console.WriteLine("Getting available instruments...");
+
             var allIInstruments = await api.GetAllInstruments();
             foreach (var instrument in allIInstruments)
             {
                 Console.WriteLine($"{instrument.Market} / {instrument.Symbol}");
             }
 
-            // Otengo los futuros de dolar
+            // Get all the dollar futures
             var dollarFutures = allIInstruments.Where( s => s.Symbol.StartsWith("DO") &&
                                                             !s.Symbol.StartsWith("DOP ")
             );
@@ -28,8 +31,10 @@ namespace Primary.Examples
                 Console.WriteLine($"{dollarFuture.Symbol}");
             }
 
-            // Obtengo los trades 
-            var sampleInstrument = allIInstruments.First(s => s.Symbol == "DONov19");
+            const string symbol = "DONov19";
+            Console.WriteLine($"Getting trades for {symbol}");
+
+            var sampleInstrument = allIInstruments.First(s => s.Symbol == symbol);
             var threeDaysAgo = DateTime.Today.AddDays(-5);
 
             foreach (var trade in await api.GetHistoricalTrades(sampleInstrument, threeDaysAgo, DateTime.Today))

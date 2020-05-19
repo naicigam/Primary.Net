16545943a9783a57283286ef2792397d3c27a499
Juan Manuel Alvarez <naicigam@gmail.com>
2020-05-19 17:23:09 -0300

    Improve order example reproducibility.

--- Primary.Examples/OrderExample.cs
+++ Primary.Examples/OrderExample.cs
@@ -1,6 +1,7 @@
 ﻿using Primary.Data;
 using Primary.Data.Orders;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,22 +14,29 @@ namespace Primary.Examples
             var api = new Api(Api.DemoEndpoint);
             await api.Login(Api.DemoUsername, Api.DemoPassword);
 
+            // Get a valid instrument and price
+            var instruments = api.GetAllInstruments().Result;
+            var instrument = instruments.Last( i => i.Symbol == Tests.Build.DollarFutureSymbol() );
+
+            var today = DateTime.Today;
+            var prices = api.GetHistoricalTrades(instrument, today.AddDays(-3), today).Result;
+
             var order = new Order
             {
-                Instrument = new Instrument { Market = "ROFX", Symbol = "DOJul20" },
+                Instrument = instrument,
                 Expiration = OrderExpiration.Day,
                 Type = OrderType.Limit,
-                Price = 78m,
-
-                Quantity = 1000,
-                Iceberg = true,
-                DisplayQuantity = 100
+                Price = prices.Last().Price,
+                Side = OrderSide.Sell,
+                Quantity = 100
             };
 
             var orderId = await api.SubmitOrder(Api.DemoAccount, order);
             Console.WriteLine($"{orderId.ClientId} / {orderId.Proprietary}");
 
             var retrievedOrder = await api.GetOrder(orderId);
+            Console.WriteLine($"{retrievedOrder.Status} / {retrievedOrder.StatusText}");
+
             Console.WriteLine($"{retrievedOrder.Quantity} / {retrievedOrder.DisplayQuantity}" +
                 $"/ {retrievedOrder.LastQuantity} / {retrievedOrder.CumulativeQuantity} / {retrievedOrder.LeavesQuantity}");
         }

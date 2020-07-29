d2b2490ea45f79d63a39d059722675fab291e9cd
Juan Manuel Alvarez <naicigam@gmail.com>
2020-07-29 20:46:41 -0300

    Move websockets their own folder and namespace

---
+++ Primary/WebSockets/MarketDataWebSocket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using Primary.Data;
+using Primary.Net;
+
+namespace Primary.WebSockets
+{
+    public class MarketDataWebSocket : WebSocket<MarketDataInfo, MarketData>
+    {
+        internal MarketDataWebSocket(MarketDataInfo marketDataToRequest, Uri url, string accessToken,
+                                     CancellationToken cancelToken)
+        : 
+        base(marketDataToRequest, url, accessToken, cancelToken)
+        {}
+    }
+}

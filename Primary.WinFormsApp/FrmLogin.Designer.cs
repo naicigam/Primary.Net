c57f8c739dbdc1f9c58f91155840cee6705db104
Federico Bridger <fbridger@gmail.com>
2020-09-21 17:16:09 -0300

    Adding Console and WinForm apps

---
+++ Primary.WinFormsApp/FrmLogin.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Primary.WinFormsApp
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(106, 12);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(255, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 41);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(106, 38);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(255, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnLogin.Location = new System.Drawing.Point(149, 64);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(103, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(258, 64);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(103, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(373, 101);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Primary Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Primary.WinFormsApp/FrmMarketData.cs
@@ -0,0 +1,85 @@
+﻿using Primary.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primary.WinFormsApp
+{
+    public partial class FrmMarketData : Form
+    {
+        
+        public FrmMarketData()
+        {
+            InitializeComponent();
+        }
+
+        public void OnMarketData(MarketData marketData)
+        {
+            try
+            {
+                this.Invoke(new Action(() => this.UpdateMarketData(marketData)));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void UpdateMarketData(MarketData marketData)
+        {
+            if (marketData.Instrument.Symbol == this.Text)
+            {
+                txtPrice.Text = marketData.Data?.Last?.Price.ToString();
+                txtChange.Text =  ((marketData.Data?.Last?.Price / marketData.Data?.Close?.Price - 1M) * 100).ToString();
+
+                var dataTable = new DataTable();
+                dataTable.Columns.Add("BidSize", typeof(decimal));
+                dataTable.Columns.Add("BidPrice", typeof(decimal));
+                dataTable.Columns.Add("OfferPrice", typeof(decimal));
+                dataTable.Columns.Add("OfferSize", typeof(decimal));
+
+                for (int i = 0; i < 5; i++)
+                {
+                    var row = dataTable.NewRow();
+                    var bid = marketData.Data?.Bids?.ElementAtOrDefault(i);
+                    if (bid != null)
+                    {
+                        row["BidSize"] = bid.Size;
+                        row["BidPrice"] = bid.Price;
+                    }
+                    var offer = marketData.Data?.Offers?.ElementAtOrDefault(i);
+                    if (offer != null)
+                    {
+                        row["OfferSize"] = offer.Size;
+                        row["OfferPrice"] = offer.Price;
+                    }
+                    dataTable.Rows.Add(row);
+                }
+
+                grdBook.DataSource = dataTable;
+            }
+
+        }
+
+        public void SetInstrument(string symbol)
+        {
+            this.Text = symbol;
+        }
+
+        private void grdBook_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FrmMarketData_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}

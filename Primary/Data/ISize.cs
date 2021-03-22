93e08d7f84901054ec749a52d30e9fb1a52b3eef
Juan Manuel Alvarez <naicigam@gmail.com>
2021-03-22 15:41:22 -0300

    Granular data types to better support web sockets data.

---
+++ Primary/Data/ISize.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primary.Data
+{
+    /// <summary>
+    /// Interface for data with a size component.
+    /// </summary>
+    interface ISize
+    {        
+        [JsonProperty("size")]
+        decimal Size { get; set; }
+    }
+}
---
+++ Primary/Data/PriceDate.cs
@@ -0,0 +1,10 @@
+﻿namespace Primary.Data
+{
+    /// <summary>
+    /// Price and date data.
+    /// </summary>
+    public class PriceDate : Date, IPrice
+    {
+        public decimal Price { get; set; }
+    }
+}

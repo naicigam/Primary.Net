4ad416a9a96e3fa238ccb7ab9cbf7ccf4d7a83e2
4 <4@everywhere.com.ar>
2023-09-10 13:10:03 -0300

    Added cache capabilities to local call of REST API "/rest/instruments/details" in new function named "GetAllInstrumentsFileCache()"

--- Primary.Examples/GetInstrumentsAndDataExample.cs
+++ Primary.Examples/GetInstrumentsAndDataExample.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+
+using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +24,25 @@ namespace Primary.Examples
             {
                 Console.WriteLine($"{instrument.Market},{instrument.Symbol},{instrument.Currency},{instrument.Description},{instrument.PriceConversionFactor}");
             }
+
+            
+            // Get instruments with file cache
+            Console.WriteLine("Getting available instruments with file cache...");
+            string cacheFile = ".\\dummy.tmp";
+            // Remove cache to test it.
+            if(System.IO.File.Exists(cacheFile)) System.IO.File.Delete(cacheFile);
+            
+            // 1st call -> Lengthy operation
+            var allIInstrumentsFileCache = await api.GetAllInstrumentsFileCache(cacheFile);
+            Trace.Assert(allIInstrumentsFileCache != null);
+            Trace.Assert(allIInstrumentsFileCache.Count() == allIInstruments.Count());
+
+            // 2do call -> Fast operation
+            var allInstrumentsFileCache2 = await api.GetAllInstrumentsFileCache(cacheFile);            
+            Trace.Assert(allIInstrumentsFileCache != null);
+            Trace.Assert(allIInstrumentsFileCache.Count() == allInstrumentsFileCache2.Count());
+
+
 
             // Get all the dollar futures and their trades
             var fiveDaysAgo = DateTime.Today.AddDays(-5);

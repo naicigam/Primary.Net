f5d41cccf610e6993acef46087171301b6f42948
Juan Manuel Alvarez <naicigam@gmail.com>
2023-12-27 20:05:25 -0300

    Remove unused test code.

--- Primary.Tests/TestWithApi.cs
+++ Primary.Tests/TestWithApi.cs
@@ -2,7 +2,6 @@
 using Primary.Data.Orders;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,43 +18,6 @@ namespace Primary.Tests
         private static readonly Lazy<Api> _lazyAnotherApi = new(
             () => Build.AnApi().WithUsername("alvarezjuandev779").WithPassword("sllsrN2$")
         );
-
-
-        protected async Task<MarketData> GetSomeMarketData()
-        {
-            if (AllInstrumentsBySymbol == null)
-            {
-                AllInstrumentsBySymbol = (await Api.GetAllInstruments()).ToDictionary(i => i.Symbol, i => i);
-            }
-
-            using var socket = Api.CreateMarketDataSocket(AllInstrumentsBySymbol.Values, new[] { Entry.Offers, Entry.Bids }, 1, 1);
-
-            MarketData retrievedData = null;
-            var dataSemaphore = new SemaphoreSlim(0, 1);
-            socket.OnData = ((_, marketData) =>
-            {
-                var instrument = AllInstrumentsBySymbol[marketData.InstrumentId.Symbol];
-
-                if (instrument.Type == InstrumentType.Equity &&
-                    (
-                        (marketData.Data.Offers != null &&
-                         marketData.Data.Offers[0].Size > instrument.MinimumTradeVolume * 2)
-                        ||
-                        (marketData.Data.Bids != null &&
-                         marketData.Data.Bids[0].Size > instrument.MinimumTradeVolume * 2)
-                    )
-                )
-                {
-                    retrievedData = marketData;
-                    dataSemaphore.Release();
-                }
-            });
-
-            await socket.Start();
-            await dataSemaphore.WaitAsync();
-
-            return retrievedData;
-        }
 
         protected async Task<OrderStatus> WaitForOrderToComplete(Api api, OrderId orderId)
         {
